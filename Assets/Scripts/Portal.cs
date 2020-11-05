0c92110e27da00613b75c70b2a944a47d871d853
Maciej N <xyanteos@gmail.com>
2020-11-05 10:29:08 +0100

    Dodany Skrypt Portal
    oraz portale z prefabu jako przyklad dzialania

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Camera myCamera;
+    public GameObject player;
+    public Transform myRenderPlane;
+    public Transform myCollidPlane;
+    public Portal otherPortal;
+    PortalCameraBehaviour portalCamera;
+    PortalTeleport portalTeleport;
+    public Material material;
+    float myAngle;
+    public void Awake()
+    {
+        portalCamera = myCamera.GetComponent<PortalCameraBehaviour>();
+
+        portalTeleport = myCollidPlane.gameObject.GetComponent<PortalTeleport>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+
+
+
+        portalCamera.playerCamera = player.gameObject.transform.GetChild(0);
+
+        portalCamera.otherPortal = otherPortal.transform;
+        portalCamera.portal = this.transform;
+
+
+        portalTeleport.player = player.transform;
+        portalTeleport.reciever = otherPortal.transform;
+        myRenderPlane.gameObject.GetComponent<Renderer>().material = Instantiate(material);
+        if (myCamera.targetTexture != null)
+        {
+            myCamera.targetTexture.Release();
+        }
+        myCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        myAngle = transform.localEulerAngles.y % 360;
+        portalCamera.SetMyAngle(myAngle);
+    }
+
+    private void Start()
+    {
+        myRenderPlane.gameObject.GetComponent<Renderer>().material.mainTexture =
+              otherPortal.myCamera.targetTexture;
+        CheckAngle();
+    }
+    void CheckAngle()
+    {
+        if (Mathf.Abs(otherPortal.ReturnMyAngle() - ReturnMyAngle()) != 180)
+        {
+            Debug.LogWarning("Portale nie są odpowiednio ustawione: "+gameObject.name);
+            Debug.Log("Angle: "+(otherPortal.ReturnMyAngle() - ReturnMyAngle()));
+        }
+    }
+    public float ReturnMyAngle()
+    {
+        return myAngle;
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/PortalCameraBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCameraBehaviour : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+    float myAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetMyAngle(float angle)
+    {
+        myAngle = angle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation,otherPortal.rotation);
+        if (myAngle == 90 || myAngle == 270)
+        {
+            angularDifferenceBetweenPortalRotations -= 90;
+        }
+        Quaternion portalRotationalDifference =
+        Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        if (myAngle == 90 || myAngle == 270)
+        {
+            newCameraDirection = new Vector3(newCameraDirection.z * -1, newCameraDirection.y,newCameraDirection.x);
+            transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        }
+        else
+        {
+            newCameraDirection = new Vector3(newCameraDirection.x * -1, newCameraDirection.y,newCameraDirection.z * -1);
+            transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        }
+    }
+
+
+
+}
